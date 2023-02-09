c934677d09f6a0befce988fc1f3d3353d88026af
Kevin Venancio <kevinsousa2012@gmail.com>
2023-02-09 17:42:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+  spaceBetween: 10,
+  slidesPerView: 3,
+  loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
